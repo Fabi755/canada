4ebc84aae8939fa65d76b421377b28f271730c8e
Fabian Keunecke <fkeunecke@frobese.de>
2015-07-24 13:03:46 +0200

    Use map instead of list

--- lib/canada.ex
+++ lib/canada.ex
@@ -1,5 +1,10 @@
 defmodule Canada do
-  defmacro can?(user, {action, _, [subject|extra_arguments]}) do
+  defmacro can?(user, {action, _, [subject]}) do
+    quote do
+      Canada.Can.can? unquote(user), unquote(action), unquote(subject)
+    end
+  end
+  defmacro can?(user, {action, _, [subject, extra_arguments]}) do
     quote do
       Canada.Can.can? unquote(user), unquote(action), unquote(subject), unquote(extra_arguments)
     end
